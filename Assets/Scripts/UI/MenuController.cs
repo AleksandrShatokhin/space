518cb388681b0068d57580cda33c48e8030faef7
dmitrysitnik <dmitry.sitnik46@gmail.com>
2022-03-12 22:26:41 +0300

    add start level

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{   
+    [SerializeField]
+    private GameObject continueGO;
+    [SerializeField]
+    private GameObject newGameGO;
+    [SerializeField]
+    private GameObject exitGO;
+
+    [SerializeField]
+    private Button continueBtn;
+    [SerializeField]
+    private Button newGameBtn;
+    [SerializeField]
+    private Button exitBtn; 
+
+    private void Start()
+    {
+        continueBtn.onClick.AddListener(Continue);
+        newGameBtn.onClick.AddListener(StartNewGame);
+        exitBtn.onClick.AddListener(ExitGame);
+    }
+    void Continue()
+    {
+
+    }
+
+    void StartNewGame(){
+        SceneManager.LoadScene("Level1");
+    }
+
+
+    void ExitGame()
+    {   
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
