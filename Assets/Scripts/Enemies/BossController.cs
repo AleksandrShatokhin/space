85e5883ec300d4845fb1d61cf131c5dd904cf5bc
Aleksandr Shatokhin <al.shatokhin2010@yandex.ru>
2022-04-14 15:27:29 +0300

    add blaster shooting for boss

--- Assets/Scripts/Enemies/BossController.cs
+++ Assets/Scripts/Enemies/BossController.cs
@@ -7,7 +7,10 @@ public class BossController : EnemyBase
     private Animator animatorCamera;
 
     private Transform targetLookAtPlayer;
-    [SerializeField] private GameObject leftGun, rightGun;
+    [SerializeField] private GameObject leftBlasterGun, rightBlasterGun;
+    [SerializeField] private GameObject spawnLeftBlasterProjectile, spawnRightBlasterProjectile;
+    [SerializeField] private GameObject blasterProjectile;
+    private bool isShotLeftBlasterGun, isShotRightBlasterGun;
 
     private Vector3 startPositionBoss, newPositionBoss;
     private float step = 0.0f;
@@ -22,25 +25,121 @@ public class BossController : EnemyBase
 
         startPositionBoss = transform.position;
         newPositionBoss = new Vector3(Random.Range(-10, 10), 0, Random.Range(6, 10));
+
+        BossShooting();
     }
 
     void Update()
     {
-        LookAtLeftGuns();
+        LookAtGuns();
         Movement();
     }
     
     // проверим относительное положение позиций player и boss и после зададим поворот бластеров в сторону player
-    private void LookAtLeftGuns()
+    private void LookAtGuns()
     {
-        Vector3 relativePosForLeftGun = targetLookAtPlayer.position - leftGun.transform.position;
-        Vector3 relativePosForRightGun = targetLookAtPlayer.position - rightGun.transform.position;
+        Vector3 relativePosForLeftGun = targetLookAtPlayer.position - leftBlasterGun.transform.position;
+        Vector3 relativePosForRightGun = targetLookAtPlayer.position - rightBlasterGun.transform.position;
 
         Quaternion rotationBossLeftGun = Quaternion.LookRotation(relativePosForLeftGun, Vector3.up);
         Quaternion rotationBossRightGun = Quaternion.LookRotation(relativePosForRightGun, Vector3.up);
 
-        leftGun.transform.rotation = rotationBossLeftGun;
-        rightGun.transform.rotation = rotationBossRightGun;
+        leftBlasterGun.transform.rotation = rotationBossLeftGun;
+        rightBlasterGun.transform.rotation = rotationBossRightGun;
+
+        AngleBounds();
+    }
+
+    // зададим условия ограничений движения пушек
+    private void AngleBounds()
+    {
+        float minAngleLeft = 35.0f, maxAngleLeft = 195.0f;
+        float minAngleRight = 160.0f, maxAngleRight = 325.0f;
+
+        // ограничиваем движение левой пушки
+        if (leftBlasterGun.transform.eulerAngles.y > maxAngleLeft)
+        {
+            leftBlasterGun.transform.rotation = Quaternion.Euler(leftBlasterGun.transform.rotation.x, maxAngleLeft, leftBlasterGun.transform.rotation.z);
+        }
+
+        if (leftBlasterGun.transform.eulerAngles.y < minAngleLeft)
+        {
+            leftBlasterGun.transform.rotation = Quaternion.Euler(leftBlasterGun.transform.rotation.x, minAngleLeft, leftBlasterGun.transform.rotation.z);
+        }
+
+        // добавим условие для стрельбы
+        if (leftBlasterGun.transform.eulerAngles.y < maxAngleLeft && leftBlasterGun.transform.eulerAngles.y > minAngleLeft)
+        {
+            isShotLeftBlasterGun = true;
+        }
+        else
+        {
+            isShotLeftBlasterGun = false;
+        }
+
+        // ограничиваем движение правой пушки
+        if (rightBlasterGun.transform.eulerAngles.y > maxAngleRight)
+        {
+            rightBlasterGun.transform.rotation = Quaternion.Euler(rightBlasterGun.transform.rotation.x, maxAngleRight, rightBlasterGun.transform.rotation.z);
+        }
+
+        if (rightBlasterGun.transform.eulerAngles.y < minAngleRight)
+        {
+            rightBlasterGun.transform.rotation = Quaternion.Euler(rightBlasterGun.transform.rotation.x, minAngleRight, rightBlasterGun.transform.rotation.z);
+        }
+
+        // добавим условие для стрельбы
+        if (rightBlasterGun.transform.eulerAngles.y < maxAngleRight && rightBlasterGun.transform.eulerAngles.y > minAngleRight)
+        {
+            isShotRightBlasterGun = true;
+        }
+        else
+        {
+            isShotRightBlasterGun = false;
+        }
+    }
+
+    // корутина левого бластера
+    IEnumerator ShotLeftBlasterGun()
+    {
+        float firstDelay = Random.Range(1, 3);
+
+        yield return new WaitForSeconds(firstDelay);
+
+        while (true)
+        {
+            if (isShotLeftBlasterGun)
+            {
+                Instantiate(blasterProjectile, spawnLeftBlasterProjectile.transform.position, spawnLeftBlasterProjectile.transform.rotation);
+            }
+
+            yield return new WaitForSeconds(Mathf.Lerp(1, 3, Random.value));
+        }
+    }
+
+    // корутина правого бластера
+    IEnumerator ShotRightBlasterGun()
+    {
+        float firstDelay = Random.Range(1, 3);
+
+        yield return new WaitForSeconds(firstDelay);
+
+        while (true)
+        {
+            if (isShotRightBlasterGun)
+            {
+                Instantiate(blasterProjectile, spawnRightBlasterProjectile.transform.position, spawnRightBlasterProjectile.transform.rotation);
+            }
+
+            yield return new WaitForSeconds(Mathf.Lerp(1, 3, Random.value));
+        }
+    }
+
+    // метод стрельбы (думаю как то собирать всё в одном месте)
+    private void BossShooting()
+    {
+        StartCoroutine(ShotLeftBlasterGun());
+        StartCoroutine(ShotRightBlasterGun());
     }
 
     //зададим движение вражеского корабля
