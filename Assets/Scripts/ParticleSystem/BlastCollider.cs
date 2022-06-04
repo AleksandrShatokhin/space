caeefa669e7435ed2c84cac845135e1dc3949a70
Aleksandr Shatokhin <al.shatokhin2010@yandex.ru>
2022-06-04 15:41:04 +0300

    fix bug, add damage to boss from rocket

--- Assets/Scripts/ParticleSystem/BlastCollider.cs
+++ Assets/Scripts/ParticleSystem/BlastCollider.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class BlastCollider : PS_RocketBlast
 {
+    private bool isDamageBoss = false;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == (int)Layers.Enemy)
         {
-            other.gameObject.GetComponent<EnemyBase>().AddDamage(damage);
+            if (other.gameObject.tag == "Boss")
+            {
+                if (!isDamageBoss)
+                {
+                    other.gameObject.GetComponent<EnemyBase>().AddDamage(damage);
+                    isDamageBoss = true;
+                }
+            }
+            else
+            {
+                other.gameObject.GetComponent<EnemyBase>().AddDamage(damage);
+            }
         }
     }
 }
